73b44a8670b5a1ecde634fdd4c3cc58684122868
vksvicky <vksvicky@gmail.com>
2025-08-06 21:42:54 +0100

    Aah, frustrating, fixing CI build failure

--- src/helpmesign/__init__.py
+++ src/helpmesign/__init__.py
@@ -8,9 +8,9 @@ __version__ = "1.0.0"
 __author__ = "HelpMeSign Team"
 __description__ = "A simple Python GUI application built with PySide6"
 
-# Don't import PySide6-dependent modules at package level
-# This prevents import errors in CI environments where GUI libraries are not available
-__all__: List[str] = []
+# Expose submodules to make them importable
+# This allows tests to import from src.helpmesign.core and src.helpmesign.utils
+__all__: List[str] = ["core", "utils", "ui", "modes", "services"]
 
 
 def get_app():
@@ -21,3 +21,31 @@ def get_app():
         return HelpMeSignApp
     except ImportError:
         return None
+
+
+# Make submodules available for import
+# This is needed for tests that import from src.helpmesign.core, etc.
+try:
+    from . import core
+except ImportError:
+    pass
+
+try:
+    from . import utils
+except ImportError:
+    pass
+
+try:
+    from . import ui
+except ImportError:
+    pass
+
+try:
+    from . import modes
+except ImportError:
+    pass
+
+try:
+    from . import services
+except ImportError:
+    pass
